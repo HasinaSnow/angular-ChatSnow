9d5073c7c7fe05c8bdac2c58243c6d4b76ae4bfa
Hasina Snow <rakotohasinasnow@gmail.com>
2025-04-16 12:10:24 +0300

    [service theme] - updated the service theme to include the signal effect

--- src/app/shared/services/theme.service.ts
+++ src/app/shared/services/theme.service.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@angular/core';
+import { effect, Injectable, signal, WritableSignal } from '@angular/core';
 
 @Injectable({providedIn: 'root'})
 export class ThemeService {
-    toggleThem() {
+    isDark: WritableSignal<boolean> = signal(false)
+    onEffect = effect(() => {
+        const isDark = this.isDark()
+        const element = document.querySelector('html')
+        isDark ? element?.classList.remove('dark') : element?.classList.add('dark')
+    })
+
+    toggleTheme() {
         const element = document.querySelector('html')
         const isDark = element?.classList.contains('dark')
         isDark ? element?.classList.remove('dark') : element?.classList.add('dark')
