bd04c483cbb3531fcefea1b3f881daca59ebb13c
Hasina Snow <rakotohasinasnow@gmail.com>
2025-04-16 12:06:36 +0300

    [component ui] - update main component

--- src/app/views/main/main.component.ts
+++ src/app/views/main/main.component.ts
@@ -1,14 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainSidebarComponent } from "./components/main-sidebar.component";
 
 @Component({
     selector: 'app-main',
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, MainSidebarComponent],
     template: `
-        <router-outlet/>
-    `
+        <div class="sm:flex h-full w-full sm:p-3 lg:p-4 xl:p-6 bg-surface-100 dark:bg-surface-800">
+            @if(desktopScreen) {
+                <!-- main sidebar -->
+                <div class="h-full hidden sm:block">
+                    <app-main-sidebar/>
+                </div>
+            }
+            <!-- main content -->
+            <div class="md:flex-1 w-full h-full sm:rounded-xl md:rounded-2xl border bg- border-surface bg-surface-0 dark:bg-surface-950">
+                <router-outlet/>
+            </div>
+        </div>`
 })
 export class MainComponent implements OnInit {
+    desktopScreen: boolean = true
     constructor() { }
 
     ngOnInit() { }
